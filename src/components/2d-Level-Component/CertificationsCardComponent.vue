fe9501a66343faee19f07d282fa8b3ca2be6b0ae
jpdzRamirez <jeremyivanpedraza@gmail.com>
2024-06-15 10:57:25 -0500

    Update bootrap+Import Components+ structure

---
+++ src/components/2d-Level-Component/CertificationsCardComponent.vue
@@ -0,0 +1,22 @@
+
+
+<script setup>
+import cert1 from "/src/assets/img/card-cert-1.jpg";
+</script>
+
+<template>
+
+
+<div class="card" style="width: 18rem;">
+    <div class="card-header bg-info">
+    Featured
+  </div>
+  <img src="/src/assets/img/card-cert-1.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+
+</template>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
 import { createApp } from 'vue'
 import './assets/scss/HV.scss'
-import App from './Production.vue'
+import App from './Welcome.vue'
+
+
+// Import Bootstrap and BootstrapVue CSS files (order is important)
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap"
+import "bootstrap-icons/font/bootstrap-icons.css"
+
 
 createApp(App).mount('#app')
